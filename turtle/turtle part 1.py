23276c5cc206a63018b59850bc8391450ec8a66d
TheWildBunch <thewildbunch3000@gmail.ru>
2021-02-17 21:11:48 +0300

    several tasks

--- turtle/turtle part 1.py
+++ turtle/turtle part 1.py
@@ -1,4 +1,5 @@
 import turtle
+import math
 
 
 def superman():
@@ -60,3 +61,42 @@ def spider():
         turtle.left(30)
 
 
+def spiral():
+    turtle.shape('turtle')
+    for i in range(1000):
+        turtle.forward(i * 0.001)
+        turtle.left(2)
+
+
+def square_spiral():
+    turtle.shape('turtle')
+    for i in range(1, 21):
+        turtle.forward(i * 10)
+        turtle.left(90)
+        turtle.forward(i * 10)
+        turtle.left(90)
+
+
+def ten_regular_polygons():
+    turtle.shape('turtle')
+    n = 3
+    r = 20 # задаем радиус первой окружности
+
+    def more_agles(n, m):  # опеределяем функцию, рисующую многоугольник
+        q = 360 / n
+        while n > 0:
+            turtle.left(q)
+            turtle.forward(m)
+            n -= 1
+
+    while n < 13:
+        m = 2 * r * math.sin(math.pi / n)  # считаем размер стороны многоугольника (a=2Rsin (360/2n))
+        x = (180 - 360 / n) / 2
+        turtle.left(x)
+        more_agles(n, m)
+        turtle.right(x)
+        turtle.penup()
+        turtle.forward(10)  # задаем расстояние м/у окружностями
+        turtle.pendown()
+        n += 1
+        r += 10  # раз расстояние м/у окружностями 10, увеличиваем радиус на 10
