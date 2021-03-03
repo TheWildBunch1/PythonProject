c07b92c9ffea2971bdf25fa4c9d6379a169ea0d5
TheWildBunch <thewildbunch3000@gmail.ru>
2021-03-03 17:24:48 +0300

    initial

---
+++ code.py
@@ -0,0 +1,31 @@
+def test_len_tuple():
+    """len() должен возвращать длину кортежа"""
+    a = (1, 2, 3, 2, 5)
+    assert len(a) == 5
+
+def test_change_tuple():
+    """Негативный тест на изменение tuple"""
+    a = (1, 2, 3, 2, 5)
+    try:
+        assert a.append(1)
+    except AttributeError:
+        pass
+
+def test_float_type():
+    """Проверка типа данных"""
+    x = 10.5
+    assert type(x) == float
+
+def test_change_str_on_float():
+    """Негативный тест на изменение str на float"""
+    a = 'Hello'
+    try:
+        assert float(a)
+    except ValueError:
+        pass
+
+if __name__ == "__main__":
+    test_len_tuple()
+    test_change_tuple
+    test_float_type()
+    test_change_str_on_float()
